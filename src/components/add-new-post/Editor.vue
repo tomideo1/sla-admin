872e5fd035a3f5b97735ad1807db1f901a5ba933
Dapo Michaels <dapomichaels@gmail.com>
2020-03-21 13:31:22 +0100

    course create

--- src/components/add-new-post/Editor.vue
+++ src/components/add-new-post/Editor.vue
@@ -2,7 +2,7 @@
   <d-card class="card-small mb-3">
     <d-card-body>
       <d-form class="add-new-post">
-        <d-input size="lg" class="mb-3" placeholder="Your Post Title" />
+        <d-input size="lg" class="mb-3" placeholder="Course Title" />
         <div ref="editor" class="add-new-post__editor mb-1"></div>
       </d-form>
     </d-card-body>
@@ -10,30 +10,37 @@
 </template>
 
 <script>
-import Quill from 'quill';
+import Quill from "quill";
+import quillEmij from "quill-emoji/dist/quill-emoji";
 
 export default {
-  name: 'editor',
+  name: "editor",
   mounted() {
-    const toolbarOptions = [
-      [{ header: [1, 2, 3, 4, 5, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      ['blockquote', 'code-block'],
-      [{ header: 1 }, { header: 2 }],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }],
-      [{ indent: '-1' }, { indent: '+1' }],
-    ];
+    const toolbarOptions = {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, false] }],
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote", "code-block"],
+        [{ header: 1 }, { header: 2 }],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ script: "sub" }, { script: "super" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        ["emoji"]
+      ],
+      handlers: { emoji: function() {} }
+    };
 
     // Init the Quill RTE
     new Quill(this.$refs.editor, {
       modules: {
         toolbar: toolbarOptions,
+        "emoji-toolbar": true,
+        "emoji-textarea": true,
+        "emoji-shortname": true
       },
-      placeholder: 'Words can be like x-rays if you use them properly...',
-      theme: 'snow',
+      placeholder: "SheLeads Africa...",
+      theme: "snow"
     });
-  },
+  }
 };
 </script>
-
