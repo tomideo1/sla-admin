a90f36605ee49c159c349977cbfe6a263d3b7966
tomideo1 <ayotomideaina@gmail.com>
2020-05-06 23:02:08 +0100

    carousel component update

--- src/views/courses/home.vue
+++ src/views/courses/home.vue
@@ -5,22 +5,8 @@
         <h6 class="text-dark title text-capitalize m-1">
           Alphabetical Order (A -Z)
         </h6>
-        <div class="scrolling-wrapper" id="content" ref="content">
-          <div
-            class="nav-circle"
-            slot="prev"
-            style="position: absolute; left: 0%; top: 40%;  "
-          >
-            <span class="prev">
-              <icon
-                name="arrow-right"
-                size="lg"
-                style="transform: rotate(180deg);"
-                @click="swipeLeft"
-              />
-            </span>
-          </div>
-          <div class="scroll m-2" v-for="i in 30">
+        <carousel refs="content">
+          <div class="scroll m-2" v-for="i in 20">
             <d-card
               :style="
                 'width:200px!important;height: 120px!important;' +
@@ -41,6 +27,7 @@
               Tag 1, Tag2 , Tag 3
             </p>
           </div>
+
           <!--            <div class="scroll" v-for="(course, idx) in courses" :key="idx">-->
           <!--              <d-card-->
           <!--                :style="-->
@@ -61,16 +48,56 @@
           <!--                {{ course.tags }}-->
           <!--              </p>-->
           <!--            </div>-->
-          <div
-            class="nav-circle "
-            slot="next"
-            style="position: absolute; right: 0%; top: 40%;"
-          >
-            <span class="next">
-              <icon name="arrow-right" size="lg" @click="swipeRight" />
-            </span>
+        </carousel>
+      </div>
+      <div class="col-lg-12">
+        <h6 class="text-dark title text-capitalize m-1">
+          Alphabetical Order (A -Z)
+        </h6>
+        <carousel refs="courses">
+          <div class="scroll m-2" v-for="i in 20">
+            <d-card
+              :style="
+                'width:200px!important;height: 120px!important;' +
+                  'backgroundImage:url(' +
+                  'https://res.cloudinary.com/dwpu7jpku/image/upload/v1558709246/samples/imagecon-group.jpg' +
+                  ');' +
+                  ' background-size:cover; background-position:center'
+              "
+            >
+            </d-card>
+
+            <h6
+              class="title text-capitalize w-75 text-dark mt-1 font-open-sans text-wrap"
+            >
+              Title
+            </h6>
+            <p class=" text-capitalize " style="color: #999999;">
+              Tag 1, Tag2 , Tag 3
+            </p>
           </div>
-        </div>
+
+          <!--            <div class="scroll" v-for="(course, idx) in courses" :key="idx">-->
+          <!--              <d-card-->
+          <!--                :style="-->
+          <!--                  'width:200px;height: 120px;' +-->
+          <!--                    'backgroundImage:url(' +-->
+          <!--                    course.cover_image +-->
+          <!--                    ');' +-->
+          <!--                    ' background-size:cover; background-position:center'-->
+          <!--                "-->
+          <!--              >-->
+          <!--              </d-card>-->
+          <!--              <h6-->
+          <!--                class="title text-capitalize w-75 text-dark mt-1 font-open-sans text-wrap"-->
+          <!--              >-->
+          <!--                {{ course.title }}-->
+          <!--              </h6>-->
+          <!--              <p class=" text-capitalize " style="color: #999999;">-->
+          <!--                {{ course.tags }}-->
+          <!--              </p>-->
+          <!--            </div>-->
+        </carousel>
       </div>
     </d-row>
     <!-- Page Header -->
@@ -80,108 +107,29 @@
 </template>
 <script>
 import { mapActions, mapGetters } from "vuex";
-import carousel from "vue-owl-carousel";
 export default {
   name: "course-home",
   data() {
-    return {};
+    return {
+      ref_data: "content"
+    };
   },
   computed: {
     ...mapGetters({
       courses: "app/getCourses"
+      // maps courses to current computed resource
     })
   },
   methods: {
-    ...mapActions("app/", ["getAllCourses"]),
-    scrollTo(element, scrollPixels, duration) {
-      const scrollPos = element.scrollLeft;
-      // Condition to check if scrolling is required
-      if (
-        !(
-          (scrollPos === 0 || scrollPixels > 0) &&
-          (element.clientWidth + scrollPos === element.scrollWidth ||
-            scrollPixels < 0)
-        )
-      ) {
-        // Get the start timestamp
-        const startTime =
-          "now" in window.performance
-            ? performance.now()
-            : new Date().getTime();
-
-        function scroll(timestamp) {
-          //Calculate the timeelapsed
-          const timeElapsed = timestamp - startTime;
-          //Calculate progress
-          const progress = Math.min(timeElapsed / duration, 1);
-          //Set the scrolleft
-          element.scrollLeft = scrollPos + scrollPixels * progress;
-          //Check if elapsed time is less then duration then call the requestAnimation, otherwise exit
-          if (timeElapsed < duration) {
-            //Request for animation
-            window.requestAnimationFrame(scroll);
-          } else {
-            return;
-          }
-        }
-        //Call requestAnimationFrame on scroll function first time
-        window.requestAnimationFrame(scroll);
-      }
-    },
-
-    swipeLeft() {
-      const content = this.$refs.content;
-      this.scrollTo(content, -300, 800);
-    },
-    swipeRight() {
-      const content = this.$refs.content;
-      this.scrollTo(content, 300, 800);
-    }
+    ...mapActions("app/", ["getAllCourses"])
+    //vuex call to get all courses
   },
   async mounted() {
     this.getAllCourses();
   },
   components: {
-    carousel,
+    Carousel: () => import("@/components/carousel"),
     Icon: () => import("@/components/SlaIcon")
   }
 };
 </script>
-
-<style scoped lang="scss">
-.scrolling-wrapper {
-  overflow-x: scroll;
-  overflow-y: hidden;
-  white-space: nowrap;
-
-  .scroll {
-    display: inline-block;
-  }
-}
-.scrolling-wrapper-flexbox {
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-
-  .scroll {
-    flex: 0 0 auto;
-  }
-}
-.scrolling-wrapper {
-  -webkit-overflow-scrolling: touch;
-}
-.scrolling-wrapper {
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  .nav-circle {
-    background: #ffffff;
-    box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.1);
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    z-index: 100 !important;
-  }
-}
-</style>
