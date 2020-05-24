3caaa2693c17db114007de1d54ceb5cb67d71bd8
tomideo1 <ayotomideaina@gmail.com>
2020-05-24 23:31:38 +0100

    dashboard charts

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,31 +1,185 @@
 <template>
   <d-container fluid class="main-content-container px-4">
     <!-- Page Header -->
-    <d-row no-gutters class="page-header py-4">
+    <d-row no-gutters class="page-header py-4 my-4">
       <d-col col sm="4" class="text-center text-sm-left mb-4 mb-sm-0">
-        <span class="text-uppercase page-subtitle">Dashboard</span>
-        <h3 class="page-title">Admin Dashboard</h3>
+        <h3 class="page-title font-poppings text-dark">Dashboard</h3>
       </d-col>
     </d-row>
-
+    <d-row>
+      <d-col class="mt-3">
+        <div class="row ">
+          <hr class="style1 col-md-4" />
+          <h5 class="font-poppings text-bold mx-auto title text-dark">
+            Overview
+          </h5>
+          <hr class="style1 col-md-4" />
+        </div>
+        <div class="row mt-3">
+          <div class="col-md-2 m-3">
+            <d-card style="border-left:7px solid #0087DB; border-radius: 0">
+              <p class="font-poppings text-dark m-2">Users</p>
+              <p class="card-number  text-center">
+                100
+              </p>
+            </d-card>
+          </div>
+          <div class="col-md-2 m-3">
+            <d-card style="border-left:7px solid #DAAA55; border-radius: 0">
+              <p class="font-poppings text-dark m-2">Engagements</p>
+              <p class="card-number  text-center">
+                23.6K
+              </p>
+            </d-card>
+          </div>
+          <div class="col-md-2 m-3">
+            <d-card style="border-left:7px solid #F77253; border-radius: 0">
+              <p class="font-poppings text-dark m-2">Messages Sent</p>
+              <p class="card-number  text-center">
+                30K
+              </p>
+            </d-card>
+          </div>
+          <div class="col-md-2 m-3">
+            <d-card style="border-left:7px solid  #1F497D; border-radius: 0">
+              <p class="font-poppings text-dark m-2">
+                Courses Completed
+              </p>
+              <p class="card-number  text-center">
+                22
+              </p>
+            </d-card>
+          </div>
+          <div class="col-md-2 m-3">
+            <d-card style="border-left:7px solid   #8D5524;  border-radius: 0">
+              <p class="font-poppings text-dark m-2">
+                Courses in Progress
+              </p>
+              <p class="card-number  text-center">
+                5
+              </p>
+            </d-card>
+          </div>
+        </div>
+      </d-col>
+    </d-row>
+    <d-row>
+      <d-card
+        class=" col-md-5 col-12  col-lg-5 "
+        style="border-radius:0!important;"
+      >
+        <p class="text-black font-poppings  m-3">
+          Login Activities
+        </p>
+        <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+        <div class="d-flex flex-row mx-auto">
+          <div class="legend_block m-1">
+            <span>
+              <div
+                class="legend_circle mr-1 "
+                style="background-color: #DAAA55;"
+              ></div>
+            </span>
+            <p>Coaches</p>
+          </div>
+          <div class="legend_block m-1">
+            <span>
+              <div
+                class="legend_circle mr-1 "
+                style="background-color: #0087DB;"
+              ></div>
+            </span>
+            <p>Users</p>
+          </div>
+        </div>
+      </d-card>
+      <d-card
+        class=" col-md-5 col-12  col-lg-5 "
+        style="border-radius:0!important;"
+      >
+        <p class="text-black font-poppings  m-3">
+          Login Activities
+        </p>
+        <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+        <div class="d-flex flex-row mx-auto">
+          <div class="legend_block m-1">
+            <span>
+              <div
+                class="legend_circle mr-1 "
+                style="background-color: #DAAA55;"
+              ></div>
+            </span>
+            <p>Coaches</p>
+          </div>
+          <div class="legend_block m-1">
+            <span>
+              <div
+                class="legend_circle mr-1 "
+                style="background-color: #0087DB;"
+              ></div>
+            </span>
+            <p>Users</p>
+          </div>
+        </div>
+      </d-card>
+    </d-row>
   </d-container>
 </template>
 
 <script>
-
-  export default {
-    name:'Dashboard',
-    components: {
-
-    },
-    data() {
-      return {
-      };
-    },
-    methods: {
-
-    },
-    computed: {}
-  };
+import { GChart } from "vue-google-charts";
+export default {
+  name: "Dashboard",
+  components: {
+    GChart
+  },
+  data() {
+    return {
+      chartData: [
+        ["Year", "Coaches", "Users"],
+        ["Dec", 20, 20],
+        ["Nov", 10, 10],
+        ["Oct", 30, 40],
+        ["Sept", 40, 45],
+        ["Aug", 50, 22],
+        ["Jul", 60, 40]
+      ],
+      chartOptions: {
+        legend: {
+          position: "none",
+          maxLines: 3,
+          fontName: "open-sans",
+          fontSize: 10
+        },
+        bar: { groupWidth: "40%" },
+        height: 200,
+        isStacked: true,
+        colors: ["#0087DB", "#DAAA55"],
+        fontName: "open-sans",
+        fontSize: 12
+      }
+    };
+  },
+  methods: {},
+  computed: {}
+};
 </script>
 
+<style lang="css" scoped>
+hr.style1 {
+  border-top: 1px solid #ababab !important;
+}
+.card-number {
+  font-weight: 300;
+  font-size: 36px;
+}
+.legend_circle {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+.legend_block > span,
+.legend_block > p {
+  display: inline-block;
+}
+</style>
--- src/views/resources/home.vue
+++ src/views/resources/home.vue
@@ -2,7 +2,7 @@
   <d-container fluid>
     <d-row>
       <div class="col-lg-6 col-md-6 col-12">
-        <h6 class="font-poppings text-grey font-weight-bold">Program</h6>
+        <h6 class="font-poppings text-grey font-weight-bold"></h6>
         <d-row>
           <div
             class="col-md-10 m-3"
@@ -13,18 +13,6 @@
               <p>{{ resource.answer }}</p>
               <icon name="pencil" size="xs" class="float-right m-2" />
               <icon name="bin" size="xs" class="float-right m-2" />
-            </resource>
-          </div>
-        </d-row>
-      </div>
-      <div class="col-lg-6 col-md-6 col-12">
-        <h6 class="font-poppings text-grey font-weight-bold">Application</h6>
-        <d-row>
-          <div class="col-md-10 m-3" v-for="i in 3">
-            <resource header="How long is the program? " color="#DAAA55;">
-              <p>All our programs last 6 months, typically between 24 weeks.</p>
-              <icon name="pencil" size="xs" class="float-right m-2" />
-              <icon name="bin" size="xs" class="float-right   m-2" />
             </resource>
           </div>
         </d-row>
