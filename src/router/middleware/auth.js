5f3f906daac54cbfd77d861c5a8f9f522f7cfabc
Benjamin Alamu <oluwaseunalamu@gmail.com>
2020-07-11 04:19:24 +0100

    Test fix

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -2,6 +2,7 @@ export default function({ store, next, router }) {
   console.log({ hhh: !store.state.auth.isLoggedIn });
   console.log({ auth: !!store.state.auth.isLoggedIn });
   if (!store.state.auth.isLoggedIn) {
+    console.log("jjj");
     return next({
       name: "login"
     });
